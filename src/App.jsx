86582dbe64a16550e37503465daf730959360263
Purushottam Sonar <sonarp02@gmail.com>
2023-12-28 21:16:27 +0530

    router,icons,tailwind installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,3 @@
-import React from "react";
-
-const App = () => {
-  return <div></div>;
-};
-
-export default App;
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
