d3defb65a8bac78855bbaffccf2a11e9c26e12a6
Purushottam Sonar <sonarp02@gmail.com>
2023-12-30 23:36:07 +0530

    home hero section done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,30 @@
 import React from "react";
 
+import Aos from "aos";
+import Hero from "../components/Hero/Hero";
+import HomeHero from "../components/Hero/HomeHero";
+
 const Home = () => {
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
+  React.useEffect(() => {
+    Aos.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    Aos.refresh();
+  }, []);
   return (
     <>
-      <h1>home</h1>
+      <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
+        {/* <Hero handleOrderPopup={handleOrderPopup} /> */}
+        <HomeHero />
+      </div>
     </>
   );
 };
