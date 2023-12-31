a6861e6da356cc4cccecb1351a9e73f5bb4d529c
Purushottam Sonar <sonarp02@gmail.com>
2023-12-31 22:13:47 +0530

    home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,13 @@ import React from "react";
 import Aos from "aos";
 import Hero from "../components/Hero/Hero";
 import HomeHero from "../components/Hero/HomeHero";
+import Products from "../components/Products/Products";
+import TopProducts from "../components/TopProducts/TopProducts";
+import Banner from "../components/Banner/Banner";
+import Subscribe from "../components/Subscribe/Subscribe";
+import Testimonials from "../components/Testimonials/Testimonials";
+import Footer from "../components/Footer/Footer";
+import Popup from "../components/Popup/Popup";
 
 const Home = () => {
   const [orderPopup, setOrderPopup] = React.useState(false);
@@ -24,6 +31,14 @@ const Home = () => {
       <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
         {/* <Hero handleOrderPopup={handleOrderPopup} /> */}
         <HomeHero />
+        <Products />
+        <TopProducts />
+        <Banner />
+        <Subscribe />
+        <Products />
+        <Testimonials />
+        <Footer />
+        <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
       </div>
     </>
   );
